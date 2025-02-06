d4186e57c78cf13ed4b4a26c629c3bbcf367a459
Ironowl1907 <facuguialtana7@gmail.com>
2025-02-06 07:40:36 -0300

    Added premake file

---
+++ libshaderc/premake5.lua
@@ -0,0 +1,38 @@
+project("ShaderC")
+kind("StaticLib")
+language("C++")
+staticruntime("off")
+warnings("off")
+
+targetdir("bin/%{prj.name}")
+objdir("bin-int/%{prj.name}")
+
+files({
+    "src/**.h",
+    "src/**.c",
+    "src/**.cc",
+})
+includedirs({})
+
+filter("system:linux")
+pic("On")
+systemversion("latest")
+
+filter("system:macosx")
+pic("On")
+
+filter("system:windows")
+systemversion("latest")
+
+filter("configurations:Debug")
+runtime("Debug")
+symbols("on")
+
+filter("configurations:Release")
+runtime("Release")
+optimize("speed")
+
+filter("configurations:Dist")
+runtime("Release")
+optimize("speed")
+symbols("off")
